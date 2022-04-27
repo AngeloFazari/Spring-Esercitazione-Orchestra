50381f7f8b7a580f39fedca740140c25a67bfa81
AngeloFazari <103179763+AngeloFazari@users.noreply.github.com>
2022-04-27 17:36:30 +0200

    project init

---
+++ src/main/java/it/fullStackAcademy/orchestra/SpringEsercitazioneOrchestraApplication.java
@@ -0,0 +1,13 @@
+package it.fullStackAcademy.orchestra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEsercitazioneOrchestraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEsercitazioneOrchestraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/fullStackAcademy/orchestra/SpringEsercitazioneOrchestraApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fullStackAcademy.orchestra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEsercitazioneOrchestraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
